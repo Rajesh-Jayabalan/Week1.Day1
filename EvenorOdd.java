37e9e006d1ab3cf16f266a9713bb09cb77e5f0c5
Rajesh-Jayabalan <88589983+Rajesh-Jayabalan@users.noreply.github.com>
2021-08-07 21:22:36 +0530

    Add files via upload

---
+++ EvenorOdd.java
@@ -0,0 +1,33 @@
+
+
+package week1.day1;
+
+
+public class EvenorOdd 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		  
+		  int i;
+		  for(i=0;i<=50;i++)
+		  {
+			  if(i%2==0)
+				{
+				  System.out.println("Even Numbers ");
+				  System.out.println(i);
+				  
+				}
+				//else
+				//{
+					//System.out.println("Odd");
+				//}
+			 
+		  }
+		  
+		
+		
+	}
+
+}
